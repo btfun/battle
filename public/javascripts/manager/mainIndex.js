c75fa30ca1cccdc4a21d209a53470a0cbe860ae2
battle <battle@mljia.cn>
2016-12-22 23:00:44 +0800

    2333

--- public/javascripts/manager/mainIndex.js
+++ public/javascripts/manager/mainIndex.js
@@ -12,8 +12,8 @@ define(function(require){
   var vueResource = require('vueResource');
   var mainElectron = require('mainElectron');//桌面方法库
 
-require('globalUri');
-require('globalUtil');
+  require('globalUri');
+  require('globalUtil');
 
  Vue.use(Vuex);
  Vue.use(VueRouter);
